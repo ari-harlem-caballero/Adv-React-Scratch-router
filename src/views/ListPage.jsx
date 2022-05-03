80a384d3e87c9a4d59391f016787607185284c56
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-05-03 12:24:50 -0700

    fetch api

--- src/views/ListPage.jsx
+++ src/views/ListPage.jsx
@@ -1,8 +1,22 @@
 // lists  Hogwarts houses (image, name)
 // useLocation, useParams
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function ListPage() {
+  const [houses, setHouses] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchHouses() {
+      const res = await fetch('https://wizard-world-api.herokuapp.com/Houses');
+      const { results } = await res.json();
+
+      setHouses(results);
+      setLoading(false);
+    }
+    fetchHouses();
+  }, []);
+
   return (
     <div>List</div>
   )
